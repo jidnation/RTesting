1c046a7d33492ed9ec9724f1b70d4330f41cd7bf
Adedola Abaru <technicatalyst78@gmail.com>
2022-12-13 22:50:24 +0100

    added the call client, modified the call folder structure

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -23,9 +23,10 @@ import 'package:reach_me/features/auth/presentation/views/signup_screen.dart';
 import 'package:reach_me/features/onboarding/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:reach_me/features/video-call/presentation/views/video_call_screen.dart';
-import 'package:reach_me/features/video-call/video_calling_screen.dart';
 import 'package:reach_me/features/voice-call/presentation/views/voice_call_screen.dart';
 import 'package:reach_me/features/voice-call/presentation/views/voice_calling_screen.dart';
+
+import '../../features/video-call/presentation/views/video_calling_screen.dart';
 
 class RMRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
