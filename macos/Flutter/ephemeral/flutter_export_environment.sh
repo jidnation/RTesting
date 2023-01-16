52ddd17aca8fd136005792c0fa8886281afc6736
jidnation <jaid1074@gmail.com>
2023-01-16 19:32:13 +0100

    fix: profile picture update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/macbook/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/macbook/Documents/app development/flutter projects/reachme_mobile"
+export "FLUTTER_ROOT=C:\Users\JidNation\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\JidNation\Documents\PROJECT\reachme_mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
