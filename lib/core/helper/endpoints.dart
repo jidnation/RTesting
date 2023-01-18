6d5d2a222ea4f2dfce7e4696a788b4c9467933c0
jidnation <jaid1074@gmail.com>
2023-01-18 11:56:08 +0100

    swicth to test server

--- lib/core/helper/endpoints.dart
+++ lib/core/helper/endpoints.dart
@@ -1,19 +1,22 @@
 class Endpoints {
   // static const String graphQLBaseUrl = "https://live.myreach.me"; //live
   // static const String gqlSubscriptionBaseUrl =
-  //     "live.myreach.me/interaction"; //live
+  //     "http://18.208.115.86:4400/graphql"; //live
   static const String graphQLBaseUrl = "https://api.myreach.me";
   static const String gqlSubscriptionBaseUrl = "ws://185.3.95.146:4000/graphql";
 
   //chat api
   static const String graphQLChatUrl = "http://185.3.95.146:4400/graphql";
-  // static const String graphQLChatUrl = "live.myreach.me/interaction"; //live
+  // static const String graphQLChatUrl =
+  //     "http://18.208.115.86:4400/graphql"; //live
+  // static const String gqlSubscriptionChatUrl =
+  //     "http://18.208.115.86:4400/graphql"; //live
   static const String gqlSubscriptionChatUrl =
       "wss://185.3.95.146:4400/graphql";
 
   //calls api
-  // static const String graphQLNotificationUrl =
-  //     'live.myreach.me/interaction'; //live
   static const String graphQLNotificationUrl =
-      'http://185.3.95.146:4500/graphql';
+      'http://18.208.115.86:4400/graphql'; //live
+  // static const String graphQLNotificationUrl =
+  //     'http://185.3.95.146:4500/graphql';
 }
